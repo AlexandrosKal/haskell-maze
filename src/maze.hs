f3a5597831c8fdfad4c2ad14e478594c1ff299b1
Alexandros Kalimeris <kalimerisalx@gmail.com>
2017-02-09 22:41:51 +0200

    Create makeMaze

--- src/maze.hs
+++ src/maze.hs
@@ -1,10 +1,11 @@
 import System.Random
 import System.IO.Unsafe
 
-data Maze = Maze { cells :: [(Bool, Bool)]  -- [(rightWall, downWall)]
-                 , width :: Int
-                 , height :: Int
-                 }
+data Maze = Maze 
+    { cells :: [(Bool, Bool)]  -- [(rightWall, downWall)]
+    , width :: Int
+    , height :: Int
+    }
 
 rand :: Int -> Int
 -- Returns a random integer from 0 to max-1
@@ -17,9 +18,22 @@ shuffle = unsafePerformIO . shuffleM
 shuffleM :: [a] -> IO [a]
 -- DON'T BOTHER! Helper for shuffle
 shuffleM [] = return []
-shuffleM n = do {
-                r <- fmap (flip mod $ length n) randomIO;
-                n1 <- return $ n !! r;
-                fmap ((:) n1) $ shuffleM $ (take r n) ++ (drop (r+1) n)
-             }
+shuffleM n = do 
+    r <- fmap (flip mod $ length n) randomIO;
+    n1 <- return $ n !! r;
+    fmap ((:) n1) $ shuffleM $ (take r n) ++ (drop (r+1) n)
 
+solvePerfect :: Maze -> (Int, Int) -> (Int, Int) -> [(Int,Int)]
+-- Uses recursive DFS to solve a perfect maze
+solvePerfect (maze c w h) (sx, sy) (gx, gy) 
+    | (sx, sy) == (gx, gy) = [(sx, sy)]
+    | fst (c !! (h * sx + sy + 1)) == False = [(sx, sy)] ++ solvePerfect maze (sx, sy + 1) (gx, gy)
+    | snd (c !! (h * sx + sy + 1)) == False = [(sx, sy)] ++ solvePerfect maze (sx + 1, sy) (gx, gy)
+
+buildLabyrinth w h sw | w > 1 = (False, False) : buildLabyrinth (w-1) h sw
+                      | w == 1 && h > 1 = (False, False) : buildLabyrinth sw (h-1) sw
+                      | w == 1 && h == 1 = [(False, False)]
+
+makeMaze w h = Maze (buildLabyrinth w h w) w h
+
+kruskal (Maze l w h) = Maze l w h
